1e7dbb1c2e00e5cfc4073eac494f9a035ddf288c
D-Itsuki <raito.kaminari@gmail.com>
2021-02-13 19:03:53 +0900

    タイトルシーンの追加、パトロールコントローラーの追加

---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    [SerializeField] GameObject lookTarget;
+    FPSController a;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float dChangeTime;
+    float dTimer;
+    [SerializeField] bool lookAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (lookAt)
+        {
+            this.transform.LookAt(lookTarget.transform);
+        }
+        this.transform.position += direction;
+        dTimer += Time.deltaTime;
+        if (dTimer > dChangeTime)
+        {
+            direction *= -1;
+            dTimer = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            a = collision.gameObject.GetComponent<FPSController>();
+            a.Damage();
+        }
+    }
+}
---
+++ Assets/Scripts/PatrolController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrolController : MonoBehaviour
+{
+    [SerializeField] Transform[] targets;
+    [SerializeField] float sDis;
+    int tNum = 0;
+    [SerializeField] float speed;
+    Rigidbody m_rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(targets[tNum]);
+        if (sDis < Vector3.Distance(this.transform.position, targets[tNum].position))
+        {
+            Vector3 dir = transform.forward;
+            Vector3 velo = dir.normalized * speed;
+            m_rb.velocity = velo;
+        }
+        else
+        {
+            tNum += 1;
+        }
+        if (tNum == targets.Length)
+        {
+            tNum = 0;
+        }
+    }
+}
